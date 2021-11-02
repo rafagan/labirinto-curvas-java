e88a65766497e0d0c7177d1b7315ad12d3be234c
Ráfagan Abreu <rafagansa@outlook.com>
2021-11-02 09:51:10 -0300

    chore: fix

--- src/main/java/br/com/camila/game/Global.java
+++ src/main/java/br/com/camila/game/Global.java
@@ -6,7 +6,7 @@ public class Global {
     public static final GlColor clearColor = new GlColor(java.awt.Color.darkGray);
     public static final GlColor defaultColor = new GlColor(java.awt.Color.white);
 
-    public static final String windowTitle = "Labirinto com Curvas by Camila";
+    public static final String windowTitle = "Curves";
     public static final int windowDefaultWidth = 1024;
     public static final int windowDefaultHeight = 768;
 
